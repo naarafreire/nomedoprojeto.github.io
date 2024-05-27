024757f1aa254e3d1d918b88d57cc54e0c19448c
Mate Toth <tothmatee97@gmail.com>
2024-05-27 22:44:28 +0100

    Merged about page and works into one homepage

---
+++ index.js
@@ -0,0 +1,46 @@
+const menuItems = document.querySelectorAll('nav ul li');
+const sections = document.querySelectorAll('.section');
+
+document.addEventListener('DOMContentLoaded', function() {
+    addScrollAnimation()
+
+    window.addEventListener('scroll', updateMenuHighlight);
+
+    // Initial check
+    updateMenuHighlight();
+});
+
+const addScrollAnimation = () => {
+    document.addEventListener('scroll', function() {
+        const fadeInImages = document.querySelectorAll('img');
+        const triggerHeight = window.innerHeight * 0.8;
+      
+        fadeInImages.forEach((el)=>{
+            const imagePosition = el.getBoundingClientRect().top;
+
+            if (imagePosition < triggerHeight) {
+              el.style.opacity = '1';
+            }
+        })
+      });
+}
+
+function updateMenuHighlight() { 
+  let currentIndex = -1;
+
+  sections.forEach((section, index) => {
+    const rect = section.getBoundingClientRect();
+
+    if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+      currentIndex = index;
+    }
+  });
+
+  menuItems.forEach((item, index) => {
+    if (index === currentIndex) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    }
+  });
+}
